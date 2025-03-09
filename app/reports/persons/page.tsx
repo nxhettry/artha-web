32e4ab555ccad75c969206c07d6cfade9dc89dc2
nxhettry <nishan.gtsn@gmail.com>
2025-03-09 22:59:32 +0545

    deleted unnecessary params in people reports

--- app/reports/persons/page.tsx
+++ app/reports/persons/page.tsx
@@ -2,26 +2,14 @@ import { getAllTransactions } from "@/lib/data";
 import React from "react";
 import TransactionList from "@/components/transaction-list";
 
-const PeopleReport = async ({
-  searchParams,
-}: {
-  searchParams: { person?: string };
-}) => {
+const PeopleReport = async () => {
   // Get all transactions
   const transactions = await getAllTransactions();
-
-  const searchPerson = await searchParams;
-  const person = searchPerson.person;
-
-  // Filter transactions based on selected person
-  const filteredTransactions = person
-    ? transactions.filter((transaction) => transaction.person === person)
-    : transactions;
 
   return (
     <main className="container mx-auto px-4 py-6">
       <h1 className="text-2xl font-bold mb-6">Person Report</h1>
-      <TransactionList initialTransactions={filteredTransactions} />
+      <TransactionList initialTransactions={transactions} />
     </main>
   );
 };
